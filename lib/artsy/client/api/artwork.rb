8e11a28070d9ed7f0744c6751fc44a962d876c70
Joey Aghion <joey@aghion.com>
2014-02-20 22:55:58 -0500

    Add basic support for creating and updating artworks

--- lib/artsy/client/api/artwork.rb
+++ lib/artsy/client/api/artwork.rb
@@ -8,7 +8,7 @@ module Artsy
         #
         # @return [Artsy::Client::Domain::Artwork]
         def artwork(id)
-          object_from_response(self, Artsy::Client::Domain::Artwork, :get, "/api/v1/artwork/#{id}", {})
+          object_from_response(self, Artsy::Client::Domain::Artwork, :get, artwork_path(id), {})
         end
 
         # Retrieves recently published artworks.
@@ -16,6 +16,26 @@ module Artsy
         # @return [Array]
         def recently_published_artworks(options = {})
           objects_from_response(self, Artsy::Client::Domain::Artwork, :get, "/api/v1/artworks/new", options)
+        end
+
+        # Create an artwork.
+        #
+        # @return [Artsy::Client::Domain::Artwork]
+        def create_artwork(params = {})
+          object_from_response(self, Artsy::Client::Domain::Artwork, :post, "/api/v1/artwork", params)
+        end
+
+        # Update an artwork.
+        #
+        # @return [Artsy::Client::Domain::Artwork]
+        def update_artwork(id, params = {})
+          object_from_response(self, Artsy::Client::Domain::Artwork, :put, artwork_path(id), params)
+        end
+
+        private
+
+        def artwork_path(id)
+          "/api/v1/artwork/#{id}"
         end
       end
     end
