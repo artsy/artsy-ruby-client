50504900420f2feef5d053038bf0177e2c3b8181
Ilya Kavalerov <ilya@artsymail.com>
2014-02-24 16:05:56 -0500

    Add helpers for creating a partner

--- lib/artsy/client/api/partner.rb
+++ lib/artsy/client/api/partner.rb
@@ -10,6 +10,13 @@ module Artsy
         def partner(id)
           object_from_response(self, Artsy::Client::Domain::Partner, :get, "/api/v1/partner/#{id}", {})
         end
+
+        # Creates a partner.
+        #
+        # @return [Artsy::Client::Domain::Partner]
+        def create_partner(params)
+          object_from_response(self, Artsy::Client::Domain::Partner, :post, "/api/v1/partner", params)
+        end
       end
     end
   end
--- spec/lib/artsy/client/api/partner_spec.rb
+++ spec/lib/artsy/client/api/partner_spec.rb
@@ -18,4 +18,18 @@ describe Artsy::Client::API::Partner do
       expect(partner.name).to eq "bitforms gallery"
     end
   end
+  describe "#create_partner" do
+    before do
+      stub_post("/api/v1/partner").to_return(
+        body: fixture("partner.json"),
+        headers: { content_type: "application/json; charset=utf-8" }
+      )
+    end
+    it "returns partner" do
+      partner = @client.create_partner(given_name: 'bitforms gallery')
+      expect(a_post("/api/v1/partner")).to have_been_made
+      expect(partner).to be_an(Artsy::Client::Domain::Partner)
+      expect(partner.name).to eq('bitforms gallery')
+    end
+  end
 end
