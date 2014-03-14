84f2f9183bf47a7be98b765c03e51a0908b2e3ba
Chung-Yi Chi <chung-yi@artsymail.com>
2014-03-14 14:52:34 -0400

    Fix api url of inquiry request.

--- lib/artsy/client/api/artwork_inquiry_request.rb
+++ lib/artsy/client/api/artwork_inquiry_request.rb
@@ -8,14 +8,14 @@ module Artsy
         #
         # @return [Artsy::Client::Domain::ArtworkInquiryRequest]
         def artwork_inquiry_requests(params = {})
-          objects_from_response(self, Artsy::Client::Domain::ArtworkInquiryRequest, :get, "/api/v1/artwork_inquiry_requests", params)
+          objects_from_response(self, Artsy::Client::Domain::ArtworkInquiryRequest, :get, "/api/v1/inquiry_requests", params)
         end
 
         # Retrieves an artwork inquiry request.
         #
         # @return [Artsy::Client::Domain::ArtworkInquiryRequest]
         def artwork_inquiry_request(id)
-          object_from_response(self, Artsy::Client::Domain::ArtworkInquiryRequest, :get, "/api/v1/artwork_inquiry_request/#{id}", {})
+          object_from_response(self, Artsy::Client::Domain::ArtworkInquiryRequest, :get, "/api/v1/inquiry_request/#{id}", {})
         end
       end
     end
--- spec/lib/artsy/client/api/artwork_inquiry_request_spec.rb
+++ spec/lib/artsy/client/api/artwork_inquiry_request_spec.rb
@@ -6,14 +6,14 @@ describe Artsy::Client::API::ArtworkInquiryRequest do
   end
   describe "#artwork_inquiry_request" do
     before do
-      stub_get("/api/v1/artwork_inquiry_request/ludvik").to_return(
+      stub_get("/api/v1/inquiry_request/ludvik").to_return(
         body: fixture("artwork_inquiry_request.json"),
         headers: { content_type: "application/json; charset=utf-8" }
       )
     end
     it "returns artwork_inquiry_request" do
       artwork_inquiry_request = @client.artwork_inquiry_request('ludvik')
-      expect(a_get("/api/v1/artwork_inquiry_request/ludvik")).to have_been_made
+      expect(a_get("/api/v1/inquiry_request/ludvik")).to have_been_made
       expect(artwork_inquiry_request).to be_an Artsy::Client::Domain::ArtworkInquiryRequest
       puts artwork_inquiry_request.email
       puts artwork_inquiry_request.message
@@ -22,14 +22,14 @@ describe Artsy::Client::API::ArtworkInquiryRequest do
   end
   describe "#artwork_inquiry_requests" do
     before do
-      stub_get("/api/v1/artwork_inquiry_requests").to_return(
+      stub_get("/api/v1/inquiry_requests").to_return(
         body: fixture("artwork_inquiry_requests.json"),
         headers: { content_type: "application/json; charset=utf-8" }
       )
     end
     it "returns artwork inquiry requests" do
       artwork_inquiry_requests = @client.artwork_inquiry_requests
-      expect(a_get("/api/v1/artwork_inquiry_requests")).to have_been_made
+      expect(a_get("/api/v1/inquiry_requests")).to have_been_made
       expect(artwork_inquiry_requests).to be_an Array
       expect(artwork_inquiry_requests.size).to eq 1
       expect(artwork_inquiry_requests.first.name).to eq "Ludvik Jahn"
